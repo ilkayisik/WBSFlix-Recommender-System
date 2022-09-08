572c2d87d75734dc2c57c89be586c80f72e738ec
ilkayisik <ilkayisik@gmail.com>
2022-09-08 10:02:12 +0200

    add more options

--- app/streamlit_recommender_app.py
+++ app/streamlit_recommender_app.py
@@ -30,7 +30,7 @@ movie_df = (
 # convert the year column to int
 movie_df['year'] = movie_df['year'].astype(int)
 
-
+# create a genre list
 genre_list = []
 for i in movie_df['genres']:
     if "|" in i:
@@ -38,16 +38,23 @@ for i in movie_df['genres']:
     else:
         genre_list.append(i)
 genre_list = list(set(genre_list))
-genre_list[0] = 'Any'
+
+i = genre_list.index("(no genres listed)")
+del genre_list[i]
+genre_list.sort()
+genre_list.insert(0, 'Any')
 
 year_list = list(set(list(movie_df['year'])))[1:]
+
+
+# create a list of movies
+movie_list = list(set(list(movie_df['title'])))
+
 # %% RATING DF
 # convert timestamp to datetime format
 rating_df['datetime'] = rating_df['timestamp'].apply(datetime.fromtimestamp)
 # drop the timestamp column
 rating_df.drop(columns=['timestamp'], inplace=True)
-
-
 # %% DEFINE FUNCTIONS
 
 
@@ -79,12 +86,38 @@ def popular_n_movies(n, genre):
     pretty_rec = df_rec.style.pipe(make_pretty)
     return pretty_rec
 
-# movie based
+# movie/item based
+def item_n_movies(movie_name, n):
+    min_rate_count = 10
+    movieId = list(movie_df[movie_df['title'] == movie_name].movieId.head(1))[0]
 
+    movies_crosstab = pd.pivot_table(data=rating_df, values='rating',
+                                     index='userId',
+                                     columns='movieId')
 
+    movie_ratings = movies_crosstab[movieId]
+    movie_ratings = movie_ratings[movie_ratings>=0] # exclude NaNs
 
-# item based
-def weighted_user_rec(user_id, n):
+    # evaluating similarity
+    similar_to_movie = movies_crosstab.corrwith(movie_ratings)
+    corr_movie = pd.DataFrame(similar_to_movie, columns=['PearsonR'])
+    corr_movie.dropna(inplace=True)
+
+    rating = pd.DataFrame(rating_df.groupby('movieId')['rating'].mean())
+    rating['rating_count'] = rating_df.groupby('movieId')['rating'].count()
+    movie_corr_summary = corr_movie.join(rating['rating_count'])
+    movie_corr_summary.drop(movieId, inplace=True) # drop forrest gump itself
+
+    top_n = movie_corr_summary[movie_corr_summary['rating_count'] >= min_rate_count].sort_values('PearsonR', ascending=False).head(n)
+    top_n = top_n.merge(movie_df, left_index=True, right_on="movieId")
+    top_n = top_n[['title', 'genres']].reset_index(drop=True)
+    new_index = ['movie-{}'.format(i+1) for i in range(n)]
+    top_n.index = new_index
+    pretty_rec = top_n.style.pipe(make_pretty)
+    return pretty_rec
+
+# user based
+def user_n_movies(user_id, n):
     users_items = pd.pivot_table(data=rating_df,
                                       values='rating',
                                       index='userId',
@@ -172,8 +205,7 @@ if pop_based_rec:
         popular_movie_recs = popular_n_movies(nr_rec, genre[0])
         st.table(popular_movie_recs)
 
-
-st.write("")
+# to put some space in between options
 st.write("")
 st.write("")
 st.write("")
@@ -181,9 +213,37 @@ st.write("")
 item_based_rec = st.checkbox("Show me a movie like this",
                              False,
                              help="Input some movies and we will show you similar ones")
+from random import choice
+short_movie_list = ['Prestige, The (2006)', 'Toy Story (1995)',
+                    'No Country for Old Men (2007)']
+if item_based_rec:
+    st.markdown("### Tell us a movie you like:")
+    with st.form(key="movie_form"):
+        movie_name = st.multiselect(label="Movie name",
+                                    # options=movie_list,
+                                    options=pd.Series(movie_list),
+                                    help="Select a movie you like",
+                                    key='item_select',
+                                    default=choice(short_movie_list))
 
+        nr_rec = st.slider("Number of recommendations",
+                           min_value=1,
+                           max_value=20,
+                           value=5,
+                           step=1,
+                           key="nr_rec_movie",
+                           help="How many movie recommendations would you like to receive?",
+                           )
 
-st.write("")
+        submit_button_movie = st.form_submit_button(label="Submit")
+
+    if submit_button_movie:
+        st.write('Because you like {}:'.format(movie_name[0]))
+
+        item_movie_recs = item_n_movies(movie_name[0], nr_rec)
+        st.table(item_movie_recs)
+
+# to put some space in between options
 st.write("")
 st.write("")
 st.write("")
@@ -219,40 +279,8 @@ if user_based_rec:
 
 
     if submit_button_user:
-        user_movie_recs = weighted_user_rec(user_id, nr_rec)
+        user_movie_recs = user_n_movies(user_id, nr_rec)
         st.table(user_movie_recs)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# user_id = st.number_input("please enter your user id", step=1, min_value=1)
-# n = st.number_input("please enter the number of recommendations you want to receive",
-#                      step=1)
-# top_recommendations = weighted_user_rec(user_id, n)
-
-# st.table(top_recommendations)
 
 
 
