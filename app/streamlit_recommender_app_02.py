bce74e56ce9a1d61843450813e1edc9a20feff00
ilkayisik <ilkayisik@gmail.com>
2022-09-08 16:50:22 +0200

    path issues

--- app/streamlit_recommender_app_02.py
+++ app/streamlit_recommender_app_02.py
@@ -22,6 +22,12 @@ from datetime import datetime
 # def install(package):
 #     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
+
+import os
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
 # %% load data
 movie_df = pd.read_csv('https://raw.githubusercontent.com/sherwan-m/WBSFLIX_Recommender_System/main/ml-latest-small/movies.csv')
 rating_df = pd.read_csv('https://raw.githubusercontent.com/sherwan-m/WBSFLIX_Recommender_System/main/ml-latest-small/ratings.csv')
